0cbdaf080bdd4761baf240629aea4e6de4146779
Timo Zander <timo---zander@live.de>
2020-12-01 12:29:22 +0100

    first commit

---
+++ day01.py
@@ -0,0 +1,14 @@
+import numpy as np
+import itertools
+
+lines = np.loadtxt("day01_input.txt")
+
+
+def solution(r):
+    mesh = np.array(list(itertools.combinations(lines, r)))
+    result = mesh[mesh.sum(axis=1) == 2020][0]
+    return (result, np.prod(result))
+
+
+print(f'Part 1: {solution(2)}')
+print(f'Part 2: {solution(3)}')
